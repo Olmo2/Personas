e0515a369beb573aee7ad71d966e4eb8332ea9e2
Olmo Tamargo <olmot.s@gmail.com>
2019-09-19 23:56:48 +0200

    Anotacion

--- src/com/olmo/arrayList/inicio/ImprimirConsola.java
+++ src/com/olmo/arrayList/inicio/ImprimirConsola.java
@@ -6,6 +6,8 @@ public class ImprimirConsola {
 	
 	public static  void Imprimir() {
 		for(int i=0;i<a.getLongitud();i++) {
+			//comprobar edad de a y si edad de a menor que la edad del mas pequeño hasta el momento, colocar a en el prmer lugar y parar la edad minima a la edad de a
+			
 			System.out.println(ListaPersonas.getPers().get(i).getNombre());
 			System.out.println(ListaPersonas.pers.get(i).getApellido());
 			System.out.println(ListaPersonas.pers.get(i).getTlf());
